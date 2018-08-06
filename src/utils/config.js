9d845d2c3ff51e076511bf732ed8625b5df6a404
Sam Wang <wym7230@gmail.com>
2018-08-06 21:44:16 +0800

    removed cryptonomic nodes from list

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,22 +5,17 @@ import languages from '../locales/languagesList'
 */
 
 const env = process.env.NODE_ENV
-let localhost
-let tezrpc
-let cryptonomicProd
-let cryptonomicTest
-
+let localhost = { name: 'localhost:8732', url: 'http://localhost:8732', type: 'betanet' }
+let tezrpc = { name: 'Tezos Betenet Network (tezrpc.me)', url: 'https://rpc.tezrpc.me', type: 'betanet' }
+let networkProviders
 if (env === 'production') {
-  localhost = { name: 'localhost:8732', url: '/local-node', type: 'betanet' }
-  tezrpc = { name: 'Tezos Betenet Network (tezrpc.me)', url: 'https://rpc.tezrpc.me', type: 'betanet' }
-  cryptonomicProd = { name: 'Tezos Betanet (cryptonomic-infra.trch)', url: '/cryptonomic-infra-prod', type: 'betanet' }
-  cryptonomicTest = { name: 'Tezos Zeronet Network (cryptonomic-infra.tech)', url: '/cryptonomic-infra-test', type: 'zeronet' }
+  networkProviders = [tezrpc]
 } else {
-  localhost = { name: 'localhost:8732', url: 'http://localhost:8732', type: 'betanet' }
-  tezrpc = { name: 'Tezos Betenet Network (tezrpc.me)', url: 'https://rpc.tezrpc.me', type: 'betanet' }
-  cryptonomicProd = { name: 'Tezos Betanet (cryptonomic-infra.trch)', url: 'https://tezos-prod.cryptonomic-infra.tech/', type: 'betanet' }
-  cryptonomicTest = { name: 'Tezos Zeronet Network (cryptonomic-infra.tech)', url: 'https://tezos-staging.cryptonomic-infra.tech', type: 'zeronet' }
+  networkProviders = [tezrpc, localhost]
 }
+
+// cryptonomicProd = { name: 'Tezos Betanet (cryptonomic-infra.trch)', url: 'https://tezos-prod.cryptonomic-infra.tech/', type: 'betanet' }
+// cryptonomicTest = { name: 'Tezos Zeronet Network (cryptonomic-infra.tech)', url: 'https://tezos-staging.cryptonomic-infra.tech', type: 'zeronet' }
 
 const menu = [
   {
@@ -46,7 +41,7 @@ const config = {
   logo: '/logo.svg',
   CORS: [],
   openPages: ['/create-wallet'],
-  networkProviders: [cryptonomicProd, cryptonomicTest, tezrpc, localhost],
+  networkProviders,
   menu,
   languages,
 }
